26c171d8721d55d5db49c1e5ae129540a0348382
Dishan77 <dishan.achar77@gmail.com>
2025-05-11 19:07:41 +0530

    fix 4

--- app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-// This root index file redirects directly to the app, completely bypassing auth
-export default function Index() {
-  return <Redirect href="/(app)" />;
-}
